ae232be9877b5ee5b1d49af69fcd4cf6da206280
2301919 <s2301919@usls.edu.ph>
2025-05-07 15:44:16 +0800

    Added Person.java

--- nano/src/Main.java
+++ nano/src/Main.java
@@ -2,8 +2,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello world!");
+        System.out.println("Hello world!");
 
+        Person person = new Person("Cruz", "Juan");
+
+        System.out.println(person.getLastName() + ", " + person.getFirstName());
 
     }
 }
