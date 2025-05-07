c31336ecd5aca6bb29ed1251bed7617f98092a19
2100111 <s2100111@usls.edu.ph>
2025-05-07 15:43:52 +0800

    Added a Pet class

--- nano/src/Main.java
+++ nano/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
         System.out.printf("Hello world!");
 
-
+        System.out.println();
+        System.out.println("Your Pet");
+        Pet pet = new Pet("Bruno", "Bulldog", 2);
+        System.out.println("Name: " + pet.getName());
+        System.out.println("Breed: " + pet.getBreed());
+        System.out.println("Age: " + pet.getAge());
     }
 }
